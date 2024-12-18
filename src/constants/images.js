701191cb6f10089ce464f9d39210d3cd83d97dc1
Sonia-Khatun <soniakhatun747818@gmail.com>
2024-12-18 20:57:02 +0530

    first commit

---
+++ src/constants/images.js
@@ -0,0 +1,46 @@
+import bg from '../assets/bg.png';
+import gallery01 from '../assets/gallery01.webp';
+import gallery02 from '../assets/gallery02.jpeg';
+import gallery03 from '../assets/gallery03.jpeg';
+import gallery04 from '../assets/gallery04.jpeg';
+import overlaybg from '../assets/overlaybg.png';
+import spoon from '../assets/spoon.svg';
+import findus from '../assets/findus.jpg';
+import laurels from '../assets/laurels.jpeg';
+import award01 from '../assets/award01.png';
+import award02 from '../assets/award02.png';
+import award03 from '../assets/award03.png';
+import award05 from '../assets/award05.png';
+import quote from '../assets/quote.png';
+import cravee from '../assets/cravee.png';
+import gg from '../assets/gg.png';
+import knifeee from '../assets/knifeee.png';
+import cocktail from '../assets/cocktail.png'
+import cchef from '../assets/cchef.jpg';
+import signature from '../assets/signature.png';
+import welcome from '../assets/welcome.jpeg'
+
+
+export default {
+  bg,
+  gallery01,
+  gallery02,
+  gallery03,
+  gallery04,
+  overlaybg,
+  spoon,
+  findus,
+  laurels,
+  award01,
+  award02,
+  award03,
+  award05,
+  quote,
+  cravee,
+  gg,
+  knifeee,
+  cocktail,
+  cchef,
+  signature,
+  welcome
+};
